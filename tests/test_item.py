87d0a5c651a6258f36c482a9c06a6db51f912c4b
Andrey Potapov <aapot@ya.ru>
2023-08-24 18:36:01 +0300

    Добавлены тесты к методам класса: instantiate_from_csv() string_to_number() setter.name()

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,6 +3,39 @@ import pytest
 from src.item import Item
 
 
+def test_name():
+    """
+    Тест сеттера класса, проверяющий длину наименования товара (не более 10 символов).
+    """
+    item = Item('Телефон', 10000, 5)
+    assert item.name == 'Телефон'
+    item.name = '012345'
+    assert item.name == '012345'
+    item.name = '0123456789остальное обрезается'
+    assert item.name == '0123456789'
+
+
+def test_instantiate_from_csv():
+    """
+    Тест на метод класса, создающий объекты из файла *.csv
+    """
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+    assert item1.price == 100
+    assert item1.quantity == 1
+
+
+def test_string_to_number():
+    """
+    Тест на статический метод класса, преобразующий строку в целое число.
+    """
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
 @pytest.fixture
 def test_item1():
     return Item('Something', 10_000.0, 13)
@@ -14,11 +47,17 @@ def test_item2():
 
 
 def test_calculate_total_price(test_item1, test_item2):
+    """
+    Тест на метод класса, рассчитывающий общую стоимость товара.
+    """
     assert test_item1.calculate_total_price() == 130_000
     assert test_item2.calculate_total_price() == 150_000
 
 
 def test_apply_discount(test_item1, test_item2):
+    """
+    Тест на метод класса, рассчитывающий цену со скидкой.
+    """
     test_item1.apply_discount()
     test_item1.apply_discount()
     assert test_item1.price == 10_000
